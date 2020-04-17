bfb6c9ad84bce54cf371951637ebce33a91ad0e6
Akshay Ravi <58636452+AkshayraviC09YC47@users.noreply.github.com>
2020-04-17 15:03:21 +0530

    Create lookup(1.1).py

---
+++ lookup(1.1).py
@@ -0,0 +1,25 @@
+import os
+import requests
+
+os.system("clear")
+
+Banner = """.____                  __                 
+|    |    ____   ____ |  | ____ ________  
+|    |   /  _ \ /  _ \|  |/ /  |  \____ \ 
+|    |__(  <_> |  <_> )    <|  |  /  |_> >
+|_______ \____/ \____/|__|_ \____/|   __/ 
+        \/                 \/     |__|    """
+
+print(Banner)
+print("<----------Coded By Copycat---------->")
+print("")
+url = input("site name: ")
+api_url = "http://api.hackertarget.com/reverseiplookup/?q="
+
+def lookup():
+	r = requests.get(api_url + url)
+	r.raise_for_status()
+	print(r.text)
+	
+lookup()
+
